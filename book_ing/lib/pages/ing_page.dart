8c97d04ef6f0d65194bd9ff45b9a82ba3f0a7f84
JazzyJiwon <101383114+JazzyJiwon@users.noreply.github.com>
2022-04-09 13:47:40 +0900

    Add files via upload

---
+++ book_ing/lib/pages/ing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class StatusofMembers extends StatefulWidget {
+  StatusofMembers({Key? key}) : super(key: key);
+
+  @override
+  State<StatusofMembers> createState() => _StatusofMembersState();
+}
+
+class _StatusofMembersState extends State<StatusofMembers> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
