7abe6fe2a3675c9d8ca2a3604aeeec240db00c4e
ck157 <46532191+ck157@users.noreply.github.com>
2022-04-20 06:59:43 -0400

    Add files via upload

--- book_ing/lib/main.dart
+++ book_ing/lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:thread/screens/homePage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF000000),
+      ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
